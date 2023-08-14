f4866669947e60d125b98d00b34448c97548ab0e
tp86 <tom0palka+tp86@gmail.com>
2023-08-14 22:24:25 +0200

    Task plugin skeleton

--- init.lua
+++ init.lua
@@ -2,11 +2,34 @@
 local core = require "core"
 local command = require "core.command"
 
-local function runtask()
-  core.log("running task")
+local tasks = {}
+
+local function runtask(projecttasks)
+  for _, task in ipairs(projecttasks) do
+    core.log("task: %s", task)
+  end
 end
 
-command.add(true, {
+local function addtasks(taskstoadd)
+  local projectdir = core.project_dir
+  local projecttasks = tasks[projectdir] or {}
+  for _, task in ipairs(taskstoadd) do
+    table.insert(projecttasks, task)
+  end
+  tasks[projectdir] = projecttasks
+end
+
+local function anyprojecttask()
+  local projectdir = core.project_dir
+  local projecttasks = tasks[projectdir] or {}
+  return #projecttasks > 0, projecttasks
+end
+
+command.add(anyprojecttask, {
   ["task:run"] = runtask,
 })
+
+return {
+  add = addtasks,
+}
 
