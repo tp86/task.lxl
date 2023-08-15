252b30ac54a7cb31f500b979ebe0105f62f7fffa
tp86 <tom0palka+tp86@gmail.com>
2023-08-15 15:20:24 +0200

    Update

--- init.lua
+++ init.lua
@@ -4,25 +4,31 @@ local command = require "core.command"
 
 local tasks = {}
 
-local function runtask(projecttasks)
-  for _, task in ipairs(projecttasks) do
-    core.log("task: %s", task)
-  end
+local function runtask(command)
+  -- execute command (in separate process)
+  -- handle errors and output
+end
+
+local function selectandruntask(projecttasks)
+  -- enter command line with project task names as suggestion
+end
+
+local function getprojecttasks()
+  local projectdir = core.project_dir
+  return tasks[projectdir] or {}
 end
 
 local function addtasks(taskstoadd)
-  local projectdir = core.project_dir
-  local projecttasks = tasks[projectdir] or {}
-  for _, task in ipairs(taskstoadd) do
-    table.insert(projecttasks, task)
+  local projecttasks = getprojecttasks()
+  for name, command in pairs(taskstoadd) do
+    projecttasks[name] = command
   end
   tasks[projectdir] = projecttasks
 end
 
 local function anyprojecttask()
-  local projectdir = core.project_dir
-  local projecttasks = tasks[projectdir] or {}
-  return #projecttasks > 0, projecttasks
+  local projecttasks = getprojecttasks()
+  return next(projecttasks) ~= nil, projecttasks
 end
 
 command.add(anyprojecttask, {
