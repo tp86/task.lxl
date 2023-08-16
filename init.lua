bd287717177884dd78b983c96201aebdd988866b
tp86 <tom0palka+tp86@gmail.com>
2023-08-16 15:37:46 +0200

    Update

--- init.lua
+++ init.lua
@@ -1,16 +1,60 @@
 -- mod-version:3
 local core = require "core"
 local command = require "core.command"
+local common = require "core.common"
 
 local tasks = {}
 
 local function runtask(command)
+  core.log("Executing task command: '%s'", command)
   -- execute command (in separate process)
-  -- handle errors and output
+  core.add_thread(function()
+    local runner = process.start({ "sh", "-c", command })
+    while runner:running() do
+      coroutine.yield(0.05)
+    end
+    local returncode = runner:returncode()
+    -- handle errors and output
+    if returncode == 0 then
+      local stdout = runner:read_stdout() or ""
+      -- TODO display stdout somehow (in separate doc? - similar to console/scm/build plugin)
+      core.log("task output: '%s'", stdout)
+    else
+      local stderr = runner:read_stderr() or ""
+      -- TODO display stderr somehow
+      core.log("task error: '%s'", stderr)
+    end
+  end)
 end
 
 local function selectandruntask(projecttasks)
-  -- enter command line with project task names as suggestion
+  local tasknames = {}
+  for name in pairs(projecttasks) do
+    table.insert(tasknames, name)
+  end
+  core.command_view:enter("Select task to run", {
+    submit = function(text, item)
+      if item then
+        runtask(item.command)
+      end
+    end,
+    suggest = function(text)
+      local res = common.fuzzy_match(tasknames, text)
+      for i, name in ipairs(res) do
+        local command = projecttasks[name]
+        local info = command
+        if #info > 64 then
+          info = info:sub(1, 63) .. "…"
+        end
+        res[i] = {
+          text = name,
+          info = info,
+          command = command,
+        }
+      end
+      return res
+    end
+  })
 end
 
 local function getprojecttasks()
@@ -36,7 +80,7 @@ local function anyprojecttask()
 end
 
 command.add(anyprojecttask, {
-  ["task:run"] = runtask,
+  ["task:run"] = selectandruntask,
 })
 
 return {
